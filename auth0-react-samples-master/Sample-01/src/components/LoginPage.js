f01b73d0710c5a2efc9f7063331009c5bb251937
Yacine Kahlerras <kahlerasse@gmail.com>
2025-04-16 21:26:11 +0100

    updating login page

--- auth0-react-samples-master/Sample-01/src/components/LoginPage.js
+++ auth0-react-samples-master/Sample-01/src/components/LoginPage.js
@@ -22,14 +22,19 @@ export default function LoginPage() {
           width: "100%",
           maxWidth: "400px",
           p: 4,
+          px: "1.5rem",
           textAlign: "center",
         }}
       >
-        <img src="/logo.svg" alt="Logo" />
-        <Typography variant="body2" color="textSecondary" sx={{ mb: 2 }}>
+        <img src="/logo.svg" alt="Logo" style={{ marginBottom: "1rem" }} />
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          sx={{ mb: 2, fontSize: "1.5rem", fontWeight: 500 }}
+        >
           Welcome
         </Typography>
-        <Typography variant="body2" color="textSecondary" sx={{ mb: 4 }}>
+        <Typography variant="body2" color="textSecondary" sx={{ mb: 1.5 }}>
           Log in to t8-staging to continue to turbodocx-staging.
         </Typography>
 
@@ -42,7 +47,15 @@ export default function LoginPage() {
           sx={{
             "& .MuiOutlinedInput-root": {
               borderRadius: "8px",
+              fontWeight: 700,
+              "& input": {
+                paddingLeft: "20px",
+              },
             },
+            "& .MuiInputLabel-shrink": {
+              transform: "translate(14px, -9px) scale(0.75)",
+            },
+            pb: 2,
           }}
         />
 
@@ -55,22 +68,26 @@ export default function LoginPage() {
               backgroundColor: "#1e4a9c",
             },
             mb: 2,
+            py: 2,
+            textTransform: "capitalize",
+            textSize: "2.5rem",
           }}
         >
           Continue
         </Button>
 
-        <Typography variant="body2" color="textSecondary">
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          sx={{ textAlign: "left" }}
+        >
           Don’t have an account?{" "}
-          <a
-            href="/register"
-            style={{ color: "#2A5CAA", textDecoration: "underline" }}
-          >
+          <a href="/register" style={{ color: "#2A5CAA", fontWeight: "700" }}>
             Sign up
           </a>
         </Typography>
 
-        <Box sx={{ display: "flex", alignItems: "center", my: 4 }}>
+        <Box sx={{ display: "flex", alignItems: "center", my: 2 }}>
           <Divider sx={{ flexGrow: 1 }} />
           <Typography variant="body2" color="textSecondary" sx={{ mx: 2 }}>
             OR
@@ -83,18 +100,27 @@ export default function LoginPage() {
           fullWidth
           sx={{
             borderColor: "gray",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: 1,
+            gap: 2,
             py: 1.5,
+            px: 2,
             borderRadius: "8px",
             "&:hover": {
               backgroundColor: "#f5f5f5",
             },
+            textTransform: "initial",
+            color: "gray",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "start",
+            textSize: "2rem",
           }}
         >
-          <Typography variant="body2">Continue with Google</Typography>
+          <img
+            src="/google.svg"
+            style={{ width: "1.5rem" }}
+            alt="google logo"
+          />
+          Continue with Google
         </Button>
       </Box>
     </Box>
